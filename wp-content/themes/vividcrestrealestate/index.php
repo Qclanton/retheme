2eca5c67c0c9269aea4b1c1177a2cb46a16c97ca
Anton Qcl <qcl.anton@gmail.com>
2015-10-08 13:23:45 -0400

    Initial commit

---
+++ wp-content/themes/vividcrestrealestate/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0.85, maximum-scale=0.85, user-scalable=0" />
+	<meta name="MobileOptimized" content="720"/>
+	<meta name="HandheldFriendly" content="true"/>
+	<?= wp_head() ?>
+</head>
+<body>
+	<header>
+    </header>
+        Content will be soon
+	<footer>
+        <?php wp_footer() ?>
+    </footer>
+</body>
+</html>
