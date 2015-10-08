ce505ca0757f09b045bc924bb702b94452886ce4
Anton Qcl <qcl.anton@gmail.com>
2015-10-08 19:39:16 -0400

    Autoload core

---
+++ wp-content/themes/vividcrestrealestate/functions.php
@@ -0,0 +1,34 @@
+<?php
+// Autoload
+spl_autoload_register(function($class) {
+    // Set params
+    $prefix = "Vividcrestrealestate";
+    $base_dir = __DIR__;
+
+    // Check namespace
+    $len = strlen($prefix);
+    
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    // Get the file
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace("\\", "/", $relative_class) . ".php";
+    
+    
+    // Require file
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+
+
+
+
+// Test
+$Properties = new \Vividcrestrealestate\Core\Properties();
+$properties = $Properties->get(1);
+// echo "<pre>"; var_dump($properties); "</pre>";
+
