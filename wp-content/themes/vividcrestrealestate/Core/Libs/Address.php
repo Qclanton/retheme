a35ac5fe0af2151e31881fb5bd080b8a62c3d41c
Anton Qcl <qcl.anton@gmail.com>
2015-10-09 14:27:56 -0400

    Added address recognizer

---
+++ wp-content/themes/vividcrestrealestate/Core/Libs/Address.php
@@ -0,0 +1,66 @@
+<?php
+namespace Vividcrestrealestate\Core\Libs;
+
+class Address 
+{   
+    public static function recognize($addrress_string) 
+    {
+        // Set default data
+        $default_info = (object)[
+            'address' => $addrress_string,
+            'country' => "Unknown",
+            'city' => "Unknown",
+        ];
+        
+        
+        
+        // Send request to api
+        $api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        $context = stream_context_create(['http'=>['timeout'=>5]]);         
+        $api_data = json_decode(@file_get_contents($api_url . urlencode($addrress_string), false, $context)); 
+        
+        
+        
+        // Return default data if request failed
+        if (
+            json_last_error() ||
+            !isset($api_data->status) || 
+            $api_data->status !== "OK" || 
+            !isset($api_data->results[0]->address_components) ||
+            !isset($api_data->results[0]->formatted_address)
+        ) {                
+            return $default_info;
+        }
+        
+        
+        
+        // Extract necessary data drom API answer
+        $info = $default_info;
+        $result = $api_data->results[0];
+        $address_components = $result->address_components;        
+        $info->address = $result->formatted_address;
+        
+        foreach ($address_components as $component) {
+            // Check object
+            if (
+                !isset($component->long_name) ||
+                !isset($component->types) ||
+                !is_array($component->types)                
+            ) {
+                continue;
+            }
+            
+            // Try to extract data            
+            if (in_array("country", $component->types)) {
+                $info->country = $component->long_name;                
+            } elseif (in_array("locality", $component->types)) {
+                $info->city = $component->long_name;  
+            }  
+        }
+        
+        
+        
+        // Return collected information
+        return $info;
+    }
+}
