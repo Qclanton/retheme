3a63b9d02bdcd6cec17eaff0fe8bf1d616386b0e
AlAyres <a.ayris@gmail.com>
2015-10-12 22:52:54 -0400

    4 cols properties for main page.
    Default view.

---
+++ wp-content/themes/vividcrestrealestate/main.php
@@ -0,0 +1,138 @@
+
+<section class="universal-wrapper content-block-wrapper">
+	<div class="universal-wrapper--inner clearfix two_cols">
+		<div class="title__border-bottom">
+			<h1>Latest Properties</h1>
+		</div>
+		<div class="universal_line-wrapper four__cols">
+			<div class="universal__cell property property--first">
+				<div class="property__image">
+					<a href="">
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</p>
+					</a>
+				</div>
+			</div>
+			<div class="universal__cell property">
+				<div class="property__image">
+					<a href="">
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</p>
+					</a>
+				</div>
+			</div>
+			<div class="universal__cell property">
+				<div class="property__image">
+					<a href="">
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</p>
+					</a>
+				</div>
+			</div>
+			<div class="universal__cell property property--last">
+				<div class="property__image">
+					<a href="">
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+						</p>
+					</a>
+				</div>
+			</div>
+		</div>
+			
+		
+		
+		
+	</div>
+</section>
+
+
+<!--
+TWO blocks content
+	<section class="universal-wrapper content-block-wrapper">
+		<div class="universal-wrapper--inner clearfix two_cols">
+			<div class="layout__col layout__col--wide">
+				
+				
+			</div>
+			<div class="layout__col layout__col--small layout__col--second">
+				<h4>Mortgage Calculator</h4>
+			</div>			
+		</div>
+	</section>
+-->
