757a85a8d6d34b702f3ed80eff6f77134ede655f
AlAyres <a.ayris@gmail.com>
2015-10-18 21:37:23 -0400

    Main Page Styles

--- wp-content/themes/vividcrestrealestate/main.php
+++ wp-content/themes/vividcrestrealestate/main.php
@@ -140,20 +140,130 @@
 			</div>
 		</div>
 		
-	</div>
-</section>
-
-
-<section class="universal-wrapper content-block-wrapper">
-	<div class="universal-wrapper--inner clearfix two_cols">
 		<div class="layout__col layout__col--wide">
 		<div class="title__border-bottom">
 			<h1>Futured Properties</h1>
 		</div>
+			
+		<div class="universal_line-wrapper three__cols">
+			<div class="universal__cell property property--first">
+				<div class="property__image">
+					<a href="">
+						<span class="label__icon--small icon--green">Open House</span>						
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+					</a>
+				</div>
+			</div>
+			<div class="universal__cell property">
+				<div class="property__image">
+					<a href="">
+						<span class="label__icon--small icon--blue">New Offer</span>
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+					</a>
+				</div>
+			</div>
+			<div class="universal__cell property">
+				<div class="property__image">
+					<a href="">
+						<span class="label__icon--small icon--green">Open House</span>
+						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+					</a>
+				</div>
+				<div class="property__info-line">
+					<a href="">
+						<p class="property__price">
+							$850 000
+						</p>
+					</a>		
+				</div>
+				<div class="property__description">
+					<a href="">
+						<ul>
+							<li>3 beds </li>
+							<li>2 baths</li>
+							<li>1500 sq.ft.</li>
+						</ul>
+					</a>
+				</div>
+			</div>
+		</div>
 
 		</div>
 		<div class="layout__col layout__col--small layout__col--second">
-			<h4>Mortgage Calculator</h4>
+			<div class="mortgage__calc mortgage__calc-main side__blocks">
+				<h4 class="title--colored">Mortgage Calculator</h4>
+				<div class="mortgage__calc-line clearfix">
+					<div class="mortgage__calc-cell">
+						<span>Mortgage Amount</span>
+						<input type="number" placeholder="Mortgage Amount" value=" " name="mortage_sum" >
+					</div>
+					<div class="mortgage__calc-cell">
+						<span>Interest Rate</span>
+						<select name="rate">
+							<option value="2.7900"> 2.79% in 1 years </option>
+							<option value="2.3400"> 2.34% in 2 years </option>
+							<option value="2.4000"> 2.40% in 3 years </option>
+							<option value="2.1000" selected="selected"> 2.10% in 5 years </option>
+						</select>
+					</div>
+					<div class="mortgage__calc-cell">
+						<span>Amortization Period</span>
+						<select name="amortization_period">
+							<option value="1"> 1 year </option>
+							<option value="2"> 2 years </option>
+							<option value="3"> 3 years </option>
+							<option value="4"> 4 years </option>
+							<option value="5"> 5 years </option>
+							<option value="6"> 6 years </option>
+						</select>
+					</div>
+					<div class="mortgage__calc-cell">
+						<span>Payment Frequency </span>
+						<select name="payment_frequency">
+							<option value="weekly" selected="selected">Weekly</option>
+							<option value="rapid_weekly">Rapid Weekly</option>
+							<option value="biweekly">Bi-Weekly</option>
+							<option value="rapid_biweekly">Rapid Bi-Weekly</option>
+							<option value="monthly">Monthly</option>
+						</select>
+					</div>	
+					<h2 class="title--underlined">Your mortgage payment would be - <strong> 889.02$ / Weekly </strong></h2>
+					<a class="blue_button universal-button" href="#" data-action="recalculate">Calculate</a>
+				</div>
+			
+			</div>
 		</div>			
 	</div>
 </section>
