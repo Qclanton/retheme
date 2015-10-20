73855bcd750681b1a80298e7c7aa8105eca04787
AlAyres <a.ayris@gmail.com>
2015-10-19 21:33:07 -0400

    Footer fixes

--- wp-content/themes/vividcrestrealestate/index.php
+++ wp-content/themes/vividcrestrealestate/index.php
@@ -91,9 +91,68 @@ $data = \Vividcrestrealestate\Core\Router::loadData($part);
     <!-- Content -->
     <?= \Vividcrestrealestate\Core\Template::renderPart($part, $data); ?>
     
+	<section class="universal-wrapper colored__line colored__line--blue">
+		<div class="universal-wrapper--inner">
+			<h1>Call us today! <a href="tel:416-939-6376">416-939-6376</a></h1>
+		</div>
+	</section>
+	
 	<footer class="universal-wrapper footer-block-wrapper">
 		<div class="universal-wrapper--inner">
-			Will be footer
+			<div class="universal_line-wrapper three__cols">
+				<div class="universal__cell footer footer--first">
+					<a href="<?= site_url(); ?>">
+						<img class="logo_footer" src="<?= get_template_directory_uri(); ?>/images/footer-logo.png" />		
+					</a>
+					<ul class="list__checked-icon">
+						<li>
+							<i class="fa fa-home"></i>
+							<p>
+								10 Walsh Avenue, Toronto, ON, M9M 1B6
+							</p>
+						</li>
+						<li>
+							<i class="fa fa-phone-square"></i>
+							<p>
+								<a href="tel:416-939-6376">416-939-6376</a>
+							</p>
+						</li>
+						<li>
+							<i class="fa fa-fax"></i>
+							<p>
+								416-747-9855
+							</p>
+						</li>
+						<li>
+							<i class="fa fa-envelope"></i>
+							<p>
+								<a href="mailto:salequick.ca@gmail.com">salequick.ca@gmail.com</a>
+							</p>
+						</li>
+					</ul>
+				</div>
+				<div class="universal__cell footer">
+					<h2>Useful Links</h2>
+					<ul class="list__checked-icon">
+						<li>
+							<i class="fa fa-link"></i>
+							<p>
+								<a href="#">How to buy your property</a>
+							</p>
+						</li>
+						<li>
+							<i class="fa fa-link"></i>
+							<p>
+								<a href="#">How to sell your property</a>
+							</p>
+						</li>
+					</ul>
+				</div>
+				
+				<div class="universal__cell footer footer--last">
+					<h2>Subscribe to Newsletter </h2>
+				</div>
+			</div>
 		</div>
 	</footer>
 	<a id="scroller" class="btn_top" href="#" style="display: none">
