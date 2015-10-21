6f17e8ca622555d6211b5d98c74d90a407f98e08
AlAyres <a.ayris@gmail.com>
2015-10-21 18:02:34 -0400

    Content page created

--- wp-content/themes/vividcrestrealestate/content.php
+++ wp-content/themes/vividcrestrealestate/content.php
@@ -1,3 +1,23 @@
- <section>
-dfdfsfd
+<section class="universal-wrapper breadcrumbs-block-wrapper">
+	<div class="universal-wrapper--inner">
+        <?= \Vividcrestrealestate\Core\Template::renderBreadcrumbs(); ?>			
+	</div>
+</section>
+
+
+<section class="universal-wrapper content-block-wrapper content-block--inner">
+	<div class="universal-wrapper--inner clearfix two_cols">
+		
+		<div class="layout__col layout__col--wide">
+			<article>
+			<div class="title__border-bottom">
+				<h1><?= $post->post_title ?></h1>
+			</div>
+				<?= $post->post_content ?>	
+			</article>
+		</div>
+		
+		<?= \Vividcrestrealestate\Core\Template::renderPart("aside"); ?>
+		
+	</div>
 </section>
