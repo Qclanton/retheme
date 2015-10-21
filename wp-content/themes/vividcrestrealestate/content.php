4dc3f01e710e6f0f12504a2b28ae221dbc3bae6c
AlAyres <a.ayris@gmail.com>
2015-10-20 21:47:44 -0400

    Testimonials. Regular + Adaptive
    Also added menu active style;
    Created content.php

---
+++ wp-content/themes/vividcrestrealestate/content.php
@@ -0,0 +1,3 @@
+ <section>
+dfdfsfd
+</section>
--- wp-content/themes/vividcrestrealestate/main.php
+++ wp-content/themes/vividcrestrealestate/main.php
@@ -141,134 +141,164 @@
 		</div>
 		
 		<div class="layout__col layout__col--wide">
-		<div class="title__border-bottom">
-			<h1>Futured Properties</h1>
-		</div>
-			
-		<div class="universal_line-wrapper three__cols">
-			<div class="universal__cell property property--first">
-				<div class="property__image">
-					<a href="">
-						<span class="label__icon--small icon--green">Open House</span>						
-						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
-					</a>
-				</div>
-				<div class="property__info-line">
-					<a href="">
-						<p class="property__price">
-							$850 000
-						</p>
-					</a>		
-				</div>
-				<div class="property__description">
-					<a href="">
-						<ul>
-							<li>3 beds </li>
-							<li>2 baths</li>
-							<li>1500 sq.ft.</li>
-						</ul>
-					</a>
-				</div>
-			</div>
-			<div class="universal__cell property">
-				<div class="property__image">
-					<a href="">
-						<span class="label__icon--small icon--blue">New Offer</span>
-						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
-					</a>
-				</div>
-				<div class="property__info-line">
-					<a href="">
-						<p class="property__price">
-							$850 000
-						</p>
-					</a>		
-				</div>
-				<div class="property__description">
-					<a href="">
-						<ul>
-							<li>3 beds </li>
-							<li>2 baths</li>
-							<li>1500 sq.ft.</li>
-						</ul>
-					</a>
-				</div>
-			</div>
-			<div class="universal__cell property">
-				<div class="property__image">
-					<a href="">
-						<span class="label__icon--small icon--green">Open House</span>
-						<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
-					</a>
-				</div>
-				<div class="property__info-line">
-					<a href="">
-						<p class="property__price">
-							$850 000
-						</p>
-					</a>		
-				</div>
-				<div class="property__description">
-					<a href="">
-						<ul>
-							<li>3 beds </li>
-							<li>2 baths</li>
-							<li>1500 sq.ft.</li>
-						</ul>
-					</a>
-				</div>
-			</div>
-		</div>
-			
-		
-		<div class="layout__col layout__col-half">
 			<div class="title__border-bottom">
-				<h2>Selling Your Property</h2>
+				<h1>Futured Properties</h1>
 			</div>
-			<article>
-				<img src="http://wpestatetheme.org/wp-content/uploads/2013/06/photodune-317366-family-having-fun-in-countryside-xs.jpg" />
-				<p>
-					The Sale Quick team–unlike any other real estate company–invests 100 percent of our time and resources on Toronto and surrounding area properties.
-				</p>
-				<p>
-					We have a total of 15+ years combined experience dealing exclusively with Toronto buyers and sellers. Over the past year, Sale Quick has maintained many happy and satisfied buyer and sellers. We’re the best at what we do.
-				</p>
-			
-			</article>
-		</div>
-		<div class="layout__col layout__col-half layout__col_half--second">
-			<div class="title__border-bottom">
-				<h2>Our Services</h2>
-			</div>
-			<article>
-				<ul class="list__checked-icon">
-					<li>
-						<i class="fa fa-check-square-o"></i>
-						<p>Provide legal services to initiate and review contracts.</p>
-					</li>
-					<li>
-						<i class="fa fa-check-square-o"></i> 
-						<p>Provide in-house marketing services to produce sales materials, including photography, brochures, and websites, etc.</p>
-					</li>
-					<li>
-						<i class="fa fa-check-square-o"></i>
-						<p>Understand Privacy–not listing with MLS ensures that only our Sales Executives have access to the lockbox.</p>
-					</li>
-					<li>
-						<i class="fa fa-check-square-o"></i>
-						<p>Ensure all repairs are complete and satisfactory and provide lists of reputable repair businesses, inspectors, major lenders and private lenders.</p>
-					</li>
-					<li>
-						<i class="fa fa-check-square-o"></i>
-						<p>Provide referred legal services to initiate and review contracts.</p>
-					</li>
-				</ul>
-						<a class="blue_button universal-button universal-button--small" data-action="recalculate" href="#">Contact us today</a>
-			</article>
-		</div>
-			
 
+			<div class="universal_line-wrapper three__cols">
+				<div class="universal__cell property property--first">
+					<div class="property__image">
+						<a href="">
+							<span class="label__icon--small icon--green">Open House</span>						
+							<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+						</a>
+					</div>
+					<div class="property__info-line">
+						<a href="">
+							<p class="property__price">
+								$850 000
+							</p>
+						</a>		
+					</div>
+					<div class="property__description">
+						<a href="">
+							<ul>
+								<li>3 beds </li>
+								<li>2 baths</li>
+								<li>1500 sq.ft.</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="universal__cell property">
+					<div class="property__image">
+						<a href="">
+							<span class="label__icon--small icon--blue">New Offer</span>
+							<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+						</a>
+					</div>
+					<div class="property__info-line">
+						<a href="">
+							<p class="property__price">
+								$850 000
+							</p>
+						</a>		
+					</div>
+					<div class="property__description">
+						<a href="">
+							<ul>
+								<li>3 beds </li>
+								<li>2 baths</li>
+								<li>1500 sq.ft.</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+				<div class="universal__cell property">
+					<div class="property__image">
+						<a href="">
+							<span class="label__icon--small icon--green">Open House</span>
+							<img src="<?= get_template_directory_uri(); ?>/images/property.jpg" />
+						</a>
+					</div>
+					<div class="property__info-line">
+						<a href="">
+							<p class="property__price">
+								$850 000
+							</p>
+						</a>		
+					</div>
+					<div class="property__description">
+						<a href="">
+							<ul>
+								<li>3 beds </li>
+								<li>2 baths</li>
+								<li>1500 sq.ft.</li>
+							</ul>
+						</a>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="layout__col layout__col-half">
+				<div class="title__border-bottom">
+					<h2>Selling Your Property</h2>
+				</div>
+				<article>
+					<img src="http://wpestatetheme.org/wp-content/uploads/2013/06/photodune-317366-family-having-fun-in-countryside-xs.jpg" />
+					<p>
+						The Sale Quick team–unlike any other real estate company–invests 100 percent of our time and resources on Toronto and surrounding area properties.
+					</p>
+					<p>
+						We have a total of 15+ years combined experience dealing exclusively with Toronto buyers and sellers. Over the past year, Sale Quick has maintained many happy and satisfied buyer and sellers. We’re the best at what we do.
+					</p>
+				</article>
+			</div>
+			<div class="layout__col layout__col-half layout__col_half--second">
+				<div class="title__border-bottom">
+					<h2>Our Services</h2>
+				</div>
+				<article>
+					<ul class="list__checked-icon">
+						<li>
+							<i class="fa fa-check-square-o"></i>
+							<p>Provide legal services to initiate and review contracts.</p>
+						</li>
+						<li>
+							<i class="fa fa-check-square-o"></i> 
+							<p>Provide in-house marketing services to produce sales materials, including photography, brochures, and websites, etc.</p>
+						</li>
+						<li>
+							<i class="fa fa-check-square-o"></i>
+							<p>Understand Privacy–not listing with MLS ensures that only our Sales Executives have access to the lockbox.</p>
+						</li>
+						<li>
+							<i class="fa fa-check-square-o"></i>
+							<p>Ensure all repairs are complete and satisfactory and provide lists of reputable repair businesses, inspectors, major lenders and private lenders.</p>
+						</li>
+						<li>
+							<i class="fa fa-check-square-o"></i>
+							<p>Provide referred legal services to initiate and review contracts.</p>
+						</li>
+					</ul>
+					<a class="blue_button universal-button universal-button--small" data-action="recalculate" href="#">Contact us today</a>
+				</article>
+			</div>
+			<div class="testimonials-wrapper">
+				<div class="title__border-bottom">
+						<h1>Client Testimonials</h1>
+					</div>
+				<div class="testimonial-wrapper">
+					
+					<div class="testimonial__image" style="background-image: url(http://wpestatetheme.org/wp-content/uploads/2013/06/testimonial-11.jpg);"></div>
+					<div class="testimonial__description">
+						<h4>
+							Susan Barkley
+						</h4>
+						<p>
+							"The Sale Quick team did an outstanding job helping me buy my first home. The high level of service and dedication to seeing things done the right way is what I look for in an agent. The Sale Quick team delivered on that expectation and I would highly recommend them to anyone who is in the market to buy a home."
+						</p>
+					</div>
+				</div>
+				<div class="testimonial-wrapper">
+					<div class="testimonial__image" style="background-image: url(http://wpestatetheme.org/wp-content/uploads/2013/05/profile-testimonial2.jpg);"></div>
+					<div class="testimonial__description">
+						<h4>
+							Lisa Simpson
+						</h4>
+						<p>
+							"We hired the Sale Quick team as our buyer agent because they are specifically trained in Short Sale Foreclosure transactions. All in all I have no doubt that we would have failed to close without an experienced representative such as the professionals at Sale Quick working for us, and we are very grateful for all they did."
+						</p>
+					</div>
+				</div>
+			</div>
+			
 		</div>
+
+		
+		
+
 		<div class="layout__col layout__col--small layout__col--second">
 			<div class="mortgage__calc mortgage__calc-main side__blocks">
 				<h4 class="title--colored">Mortgage Calculator</h4>
@@ -312,6 +342,8 @@
 				</div>
 			
 			</div>
-		</div>			
+
+		</div>	
+		
 	</div>
 </section>
